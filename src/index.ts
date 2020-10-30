a9ff5ab2b8d14fea19e67de82764a8c47d28e12c
Stephen Buchanan <stephen.buchanan@random.studio>
2020-10-30 10:58:38 +0100

    init commit, possibly working

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+const remap = (value: number, start1: number, stop1: number, start2: number, stop2: number, clamp: boolean = false): number => {
+  let outgoing = (value - start1) / (stop1 - start1) * (stop2 - start2) + start2;
+  if (clamp === true) {
+      if (outgoing > stop2) { 
+        return stop2;
+      } else if (outgoing < start2) { 
+        return start2
+      } else {
+        return outgoing
+      }
+  } else {
+    return outgoing;
+  }
+}
+
