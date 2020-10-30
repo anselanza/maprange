377bb7826e79cc55e0be7e97b99435c300e20595
Stephen Buchanan <stephen.buchanan@random.studio>
2020-10-30 11:16:35 +0100

    more scripts

--- src/index.test.ts
+++ src/index.test.ts
@@ -8,4 +8,10 @@ describe('percentage examples', () => {
     expect(result).toBe(50);
   })
 
+  test('0.1 is 10%', () => {
+    const value = 0.1;
+    const result = remap(value, 0, 1, 0, 100);
+    expect(result).toBe(10);
+  }
+
 })
