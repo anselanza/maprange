49590bec93438c8fdc310f75a14cf8cf280c605f
Stephen Buchanan <stephen.buchanan@random.studio>
2020-10-30 11:26:40 +0100

    way more tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -15,3 +15,33 @@ describe('percentage examples', () => {
   })
 
 })
+
+describe('pixel values', () => {
+  test('far left', () => {
+    const value = 0;
+    expect(remap(value, 0, 1, 0, 1920)).toBe(0)
+  })
+  test('leftish', () => {
+    const value = 0.25;
+    expect(remap(value, 0, 1, 0, 1920)).toBe(480)
+  })
+  test('far right', () => {
+    const value = 1.0;
+    expect(remap(value, 0, 1, 0, 1920)).toBe(1920)
+  })
+})
+
+describe('negative values', () => {
+  test('middle is 0', () => {
+    const value = 0.5;
+    expect(remap(value, 0, 1, -1, 1)).toBe(0);
+  })
+  test('leftish', () => {
+    const value = 0.25;
+    expect(remap(value, 0, 1, -1000, 1000)).toBe(-500)
+  })
+  test('far right', () => {
+    const value = 80;
+    expect(remap(value, 0, 100, -100, 100)).toBe(60)
+  })
+})
