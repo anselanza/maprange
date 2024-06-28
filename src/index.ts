0e543638d48a6835333d5a04ca1352c1413f93f0
Stephen Buchanan <webtvmaster@gmail.com>
2024-06-28 13:35:23 +0200

    export simpler way

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ const checkValidRanges = (arrays: number[][]): boolean =>
   arrays.reduce((result, a) => a.length !== 2  ? false : result, true)
 
 /** Remap an input `value` which is currently in the range `inputRange` to a new range `outputRange` */
-const remap = (value: number, inputRange: number[], targetRange: number[], clamp: boolean = false, shouldRound: boolean = false): number => {
+export const remap = (value: number, inputRange: number[], targetRange: number[], clamp: boolean = false, shouldRound: boolean = false): number => {
   if (!checkValidRanges([inputRange, targetRange])) {
     throw Error('inputRange and targetRange must be number arrays with exactly 2 elements, and these must differ; you gave:' 
     + JSON.stringify({inputRange, targetRange}));
@@ -29,7 +29,7 @@ const remap = (value: number, inputRange: number[], targetRange: number[], clamp
 }
 
 /** Remap all values in the array `values` from `inputRange` to `outputRange`; returns the remapped array */
-const remapArray = (values: number[], inputRange: number[], targetRange: number[], clamp: boolean = false, shouldRound: boolean = false): number[] => 
+export const remapArray = (values: number[], inputRange: number[], targetRange: number[], clamp: boolean = false, shouldRound: boolean = false): number[] => 
   values.reduce((result, v) => [
     ...result,
     remap(v, inputRange, targetRange, clamp, shouldRound)
@@ -38,15 +38,9 @@ const remapArray = (values: number[], inputRange: number[], targetRange: number[
   /** Works the same as remapArray, but conveniently allows you to simply specify the input and output
    * dimensions (width and height), and assumes that the min for each dimension in each range is zero.
    */
-const remapCoords = (inputCoords: number[], inputDimensions: number[], targetDimensions: number[], clamp=false, shouldRound = false): number[] => {
+export const remapCoords = (inputCoords: number[], inputDimensions: number[], targetDimensions: number[], clamp=false, shouldRound = false): number[] => {
   if (inputCoords.length !== targetDimensions.length || inputCoords.length !== inputDimensions.length) {
     throw Error('coordinates must have same number of dimensions as input and target dimensions')
   }
   return inputCoords.map((x, index) => remap(x, [0, inputDimensions[index]], [0, targetDimensions[index]], clamp, shouldRound))
-}
-
-export {
-  remap,
-  remapArray,
-  remapCoords
 }
